df53fcfade5aefbdda7b1e8f6d58d164f2653c7b
Sualp Say <sayahmetsualp@gmail.com>
2024-01-31 17:48:33 +0300

    Initialize

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use machine learning to predict grocery sales',
+    author='Sualp Say',
+    license='',
+)
