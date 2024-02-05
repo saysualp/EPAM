36e7c4674d223b502d4b7fe2ef35ce787fc9ffea
Sualp Say <sayahmetsualp@gmail.com>
2024-02-05 13:47:38 +0300

    better write the app

--- app.py
+++ app.py
@@ -10,23 +10,45 @@ from src.models.predict_model import predict_model
 from src.visualization.visualize import visualize
 from pathlib import Path
 
+
 def load_hydra_config(config_path):
     cfg = OmegaConf.load(config_path)
     return cfg
 
+
 def load_eda_data():
-    comp_dir = Path('data/processed/notebooks')
-    return pd.read_csv(comp_dir / 'train.csv', parse_dates=['date'], infer_datetime_format=True)
+    comp_dir = Path("data/processed/notebooks")
+    return pd.read_csv(
+        comp_dir / "train.csv",
+        parse_dates=["date"],
+        infer_datetime_format=True,
+    )
+
 
 def run_forecast_flow(cfg, fh_selection, id_selection, show_actuals):
-    with st.spinner("Model is training and forecasts are being made, please wait..."):
+    with st.spinner(
+        "Model is training and forecasts are being made, please wait..."
+    ):
         make_dataset(cfg)
         model_version = build_features(cfg, forecast_horizon=fh_selection)
-        train_model(cfg, model_version=model_version, forecast_horizon=fh_selection)
-        predict_model(cfg, model_version=model_version, forecast_horizon=fh_selection)
-        fig = visualize(cfg, id=id_selection, model_version=model_version, show_actuals=show_actuals)
-        st.markdown("<h3 style='text-align: center;'>Forecast Results</h3>", unsafe_allow_html=True)
+        train_model(
+            cfg, model_version=model_version, forecast_horizon=fh_selection
+        )
+        predict_model(
+            cfg, model_version=model_version, forecast_horizon=fh_selection
+        )
+        fig = visualize(
+            cfg,
+            id=id_selection,
+            model_version=model_version,
+            show_actuals=show_actuals,
+        )
+        st.markdown(
+            "<h3 style='text-align: center;'>Forecast Results</h3>",
+            unsafe_allow_html=True,
+        )
         st.plotly_chart(fig)
+
 
 def render_forecast_tab(cfg):
     fh_options = [28, 42, 60, 90]
@@ -40,17 +62,26 @@ def render_forecast_tab(cfg):
         col1, col2 = st.columns(2)
 
         with col1:
-            fh_selection = st.select_slider("Select forecast length in days:", options=fh_options, value=fh_options[0])
+            fh_selection = st.select_slider(
+                "Select forecast length in days:",
+                options=fh_options,
+                value=fh_options[0],
+            )
 
         with col2:
-            id_selection = st.selectbox("Select family-store pair ID:", id_options)
+            id_selection = st.selectbox(
+                "Select family-store pair ID:", id_options
+            )
 
         # Toggle for showing actual values
         show_actuals = st.checkbox("Show actual values", value=False)
 
         # Display success message if the toggle is activated
         if show_actuals:
-            st.success("Actual sales for the forecast period will be shown!", icon="📈")
+            st.success(
+                "Actual sales for the forecast period will be shown!",
+                icon="📈",
+            )
 
     if st.button("Forecast"):
         if not id_selection:
@@ -58,60 +89,127 @@ def render_forecast_tab(cfg):
         else:
             run_forecast_flow(cfg, fh_selection, id_selection, show_actuals)
 
+
 def render_eda_tab():
     data = load_eda_data()
-    
-    daily = data.groupby('date').agg({'sales': 'sum', 'dcoilwtico': 'mean'})
-    daily['year'] = daily.index.year
-    daily['day_of_year'] = daily.index.dayofyear
-    daily['smooth7_sales'] = daily['sales'].rolling(window=7,  center=True, min_periods=3).mean()
-    daily['smooth30_sales'] = daily['sales'].rolling(window=30,  center=True, min_periods=15).mean()
-    daily['smooth365_sales'] = daily['sales'].rolling(window=365,  center=True, min_periods=183).mean()
+
+    daily = data.groupby("date").agg({"sales": "sum", "dcoilwtico": "mean"})
+    daily["year"] = daily.index.year
+    daily["day_of_year"] = daily.index.dayofyear
+    daily["smooth7_sales"] = (
+        daily["sales"].rolling(window=7, center=True, min_periods=3).mean()
+    )
+    daily["smooth30_sales"] = (
+        daily["sales"].rolling(window=30, center=True, min_periods=15).mean()
+    )
+    daily["smooth365_sales"] = (
+        daily["sales"].rolling(window=365, center=True, min_periods=183).mean()
+    )
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=daily.index, y=daily['sales'], mode='lines', name='Daily Sales'))
-    fig.add_trace(go.Scatter(x=daily.index, y=daily['smooth7_sales'], mode='lines', name='7-day Moving Average'))
-    fig.add_trace(go.Scatter(x=daily.index, y=daily['smooth30_sales'], mode='lines', name='30-day Moving Average'))
-    fig.add_trace(go.Scatter(x=daily.index, y=daily['smooth365_sales'], mode='lines', name='365-day Moving Average', line=dict(color='red')))
-    fig.update_layout(title='Overall Sales Trend', xaxis_title='Date', yaxis_title='Sales (Qty)')
+    fig.add_trace(
+        go.Scatter(
+            x=daily.index, y=daily["sales"], mode="lines", name="Daily Sales"
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=daily.index,
+            y=daily["smooth7_sales"],
+            mode="lines",
+            name="7-day Moving Average",
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=daily.index,
+            y=daily["smooth30_sales"],
+            mode="lines",
+            name="30-day Moving Average",
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=daily.index,
+            y=daily["smooth365_sales"],
+            mode="lines",
+            name="365-day Moving Average",
+            line=dict(color="red"),
+        )
+    )
+    fig.update_layout(
+        title="Overall Sales Trend",
+        xaxis_title="Date",
+        yaxis_title="Sales (Qty)",
+    )
     st.plotly_chart(fig)
 
-    yoy = daily.groupby(['year', 'day_of_year']).agg({'smooth7_sales': 'sum'}).reset_index()
-    fig = px.line(yoy, x='day_of_year', y='smooth7_sales', color='year')
-    fig.update_layout(title='YoY Smoothed Sales Plot', xaxis_title='Day of Year', yaxis_title='Sales (Qty)')
+    yoy = (
+        daily.groupby(["year", "day_of_year"])
+        .agg({"smooth7_sales": "sum"})
+        .reset_index()
+    )
+    fig = px.line(yoy, x="day_of_year", y="smooth7_sales", color="year")
+    fig.update_layout(
+        title="YoY Smoothed Sales Plot",
+        xaxis_title="Day of Year",
+        yaxis_title="Sales (Qty)",
+    )
     st.plotly_chart(fig)
 
-    pg = data.groupby(['date', 'family']).agg({'sales': 'sum'}).reset_index()
-    pg['smooth7_sales'] = pg.groupby('family')['sales'].rolling(window=7, center=True, min_periods=3).mean().reset_index(level=0, drop=True)
-    fig = px.line(pg, x='date', y='smooth7_sales', color='family')
-    fig.update_layout(title='Smoothed Sales Over Time by Family', xaxis_title='Date', yaxis_title='Sales (Qty)')
+    pg = data.groupby(["date", "family"]).agg({"sales": "sum"}).reset_index()
+    pg["smooth7_sales"] = (
+        pg.groupby("family")["sales"]
+        .rolling(window=7, center=True, min_periods=3)
+        .mean()
+        .reset_index(level=0, drop=True)
+    )
+    fig = px.line(pg, x="date", y="smooth7_sales", color="family")
+    fig.update_layout(
+        title="Smoothed Sales Over Time by Family",
+        xaxis_title="Date",
+        yaxis_title="Sales (Qty)",
+    )
     st.plotly_chart(fig)
+
 
 def main():
-    cfg = load_hydra_config('config/config.yaml')
+    cfg = load_hydra_config("config/config.yaml")
 
     # Main App Logic
     st.set_page_config(page_title="Sales Prediction Tool")
 
     tabs = ["Forecast Tool", "Descriptives"]
 
-    st.sidebar.markdown("""
+    st.sidebar.markdown(
+        """
     <style>
     .big-font {
         font-size:20px !important;
     }
     </style>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
     # Using the custom CSS class
-    st.sidebar.markdown('<p class="big-font">Navigate</p>', unsafe_allow_html=True)
+    st.sidebar.markdown(
+        '<p class="big-font">Navigate</p>', unsafe_allow_html=True
+    )
     tab = st.sidebar.radio("", tabs)
 
     if tab == "Forecast Tool":
-        st.markdown("<h1 style='text-align: center;'>Chain Level Forecast</h1>", unsafe_allow_html=True)
-        st.write("The section provides an interactive platform for generating sales forecasts across various family-store pairings within the retail chain.", unsafe_allow_html=True)
+        st.markdown(
+            "<h1 style='text-align: center;'>Chain Level Forecast</h1>",
+            unsafe_allow_html=True,
+        )
+        st.write(
+            "The section provides an interactive platform for generating sales forecasts across various family-store pairings within the retail chain.",
+            unsafe_allow_html=True,
+        )
 
-        st.info("""
+        st.info(
+            """
         **Here’s how one can leverage this tool:**
 
         - **Forecast Length Selection:** Select the desired forecast length in days from slider.
@@ -121,18 +219,29 @@ def main():
         - **Actual Values Comparison:** An option is available to display actual sales figures alongside the forecasted data for comparison.
 
         Upon making these selections, clicking on the "Forecast" button generates the sales forecasts.
-        """, icon="🔍")
+        """,
+            icon="🔍",
+        )
         render_forecast_tab(cfg)
 
     elif tab == "Descriptives":
-        st.markdown("<h1 style='text-align: center;'>Exploratory Data Analysis</h1>", unsafe_allow_html=True)
-        st.write("""
+        st.markdown(
+            "<h1 style='text-align: center;'>Exploratory Data Analysis</h1>",
+            unsafe_allow_html=True,
+        )
+        st.write(
+            """
         This section highlights some efforts from the exploratory data analysis that give an overall picture of the business's performance.
                 
         The aim is to provide a general overview, showing where the business stands and how different segments compare. This includes looking at sales over time, understanding which families are popular, and spotting any significant changes from one period to the next.
-        """)
-        st.info("""It's important to note that the dashboards presented are just a part of the exploratory analysis. They've been selected to offer useful insights without overwhelming users with too much information.""", icon="📊")
+        """
+        )
+        st.info(
+            """It's important to note that the dashboards presented are just a part of the exploratory analysis. They've been selected to offer useful insights without overwhelming users with too much information.""",
+            icon="📊",
+        )
         render_eda_tab()
+
 
 if __name__ == "__main__":
     main()
